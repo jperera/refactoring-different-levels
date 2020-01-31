0d887342a95cf7e242ffdaeb89818035d471348a
Jaime Perera <jaimejperera@gmail.com>
2020-01-31 18:30:39 +0100

    Initial Spring Boot skeleton

---
+++ src/main/java/org/jperera/refactoringdifferentlevels/RefactoringDifferentLevelsApplication.java
@@ -0,0 +1,13 @@
+package org.jperera.refactoringdifferentlevels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringDifferentLevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringDifferentLevelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jperera/refactoringdifferentlevels/RefactoringDifferentLevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jperera.refactoringdifferentlevels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringDifferentLevelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
